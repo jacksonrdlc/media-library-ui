763df14d2e45179c53f31c7b515836c75a8d85de
Jack Rudelic <jack.rudelic@wwt.com>
2018-03-09 07:44:14 -0600

    cut down list and css changes

--- src/MediaList.js
+++ src/MediaList.js
@@ -5,20 +5,17 @@ const movieList = require('./movies.json');
 
 export class MediaList extends React.Component {
   render() {
-    const {movie} = this.props;
 
     var listItems = movieList.map(function (item) {
       return (
-        <li>
           <MediaItem item={item}></MediaItem>
-        </li>
       );
     });
 
     return (
-      <ul>
+      <p>
         {listItems}
-      </ul>
+      </p>
     );
   }
 }
